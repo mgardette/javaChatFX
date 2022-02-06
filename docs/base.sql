0aab29a2d07f1935e431ca2daeef1900eb77745a
ncoupey <74413792+ncoupey@users.noreply.github.com>
2022-02-06 15:30:37 +0100

    remove email from sign up

--- docs/base.sql
+++ docs/base.sql
@@ -1,8 +1,12 @@
+DROP TABLE Utilisateur;
+DROP TABLE Jeu;
+DROP TABLE ScoreJeu;
+
+
 CREATE TABLE Utilisateur (
     num_uti int CONSTRAINT pk_num_uti PRIMARY KEY,
-    pseudo varchar2(30) NOT NULL,
+    pseudo varchar2(30) UNIQUE NOT NULL,
     mdp varchar2(30) NOT NULL,
-    mail varchar2(100) NOT NULL,
     photo_profil varchar2(255) DEFAULT 'no_profile',
     statut varchar2(200)
 );
@@ -17,8 +21,10 @@ CREATE TABLE ScoreJeu (
     nb_victoires int,
     nb_parties int,
     joueur int,
-    adversaire int
+    adversaire int,
+    num_jeu int
 );
 
+ALTER TABLE ScoreJeu ADD CONSTRAINT fk_num_jeu FOREIGN KEY (num_jeu) REFERENCES Jeu(num_jeu);
 ALTER TABLE ScoreJeu ADD CONSTRAINT fk_joueur FOREIGN KEY (joueur) REFERENCES Utilisateur(num_uti);
 ALTER TABLE ScoreJeu ADD CONSTRAINT fk_adversaire FOREIGN KEY (adversaire) REFERENCES Utilisateur(num_uti);
--- src/gui/NewAccountController.java
+++ src/gui/NewAccountController.java
@@ -29,9 +29,6 @@ public class NewAccountController {
     private Button create_button;
 
     @FXML
-    private TextField mail_input;
-
-    @FXML
     private PasswordField new_pass;
 
     @FXML
@@ -52,7 +49,7 @@ public class NewAccountController {
     	if(verif_saisie(stm)) {
     		ResultSet rs = stm.executeQuery("SELECT max(num_uti) + 1 as id FROM utilisateur");
         	rs.next();
-        	rs = stm.executeQuery("INSERT INTO utilisateur VALUES(" + rs.getInt("id") + ", '" + new_username.getText() + "', '" + new_pass.getText() + "', '" + mail_input.getText() + "', null)");
+        	rs = stm.executeQuery("INSERT INTO utilisateur VALUES(" + rs.getInt("id") + ", '" + new_username.getText() + "', '" + new_pass.getText() + "', default, null)");
         	currentWindow.close();
         	rs.close();
     	}
@@ -66,7 +63,7 @@ public class NewAccountController {
 		pass_error.setVisible(false);
 		confirm_error.setVisible(false);
 		
-    	if(new_username.getText().equals("") || new_pass.getText().equals("") || mail_input.getText().equals("")) {
+    	if(new_username.getText().equals("") || new_pass.getText().equals("")) {
     		username_error.setText("Veuillez remplir tout les champs.");
     		username_error.setVisible(true);
     	}

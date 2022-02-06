640a50b6192c523a594cbe35708d2e9521ebfdfd
ncoupey <74413792+ncoupey@users.noreply.github.com>
2022-02-06 05:13:18 +0100

    init Jeu

--- src/game/Jeu.java
+++ src/game/Jeu.java
@@ -2,12 +2,13 @@ package game;
 
 public class Jeu {
 	
-	private int[][] plateau = new int[6][7];
+	private Plateau plateau;
 	private String j1;
 	private String j2;
 	private String joueurActif = j1;
 	
 	public Jeu(String j1, String j2) {
+		this.plateau = new Plateau();
 		this.j1 = j1;
 		this.j2 = j2;
 	}
